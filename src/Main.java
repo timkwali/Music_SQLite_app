d46e0eabcdd72a09b94b4b697373d89815100cd5
timkwali <timkwalit@accessbankplc.com>
2023-11-17 15:03:04 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import model.DataSource;
+
+public class Main {
+    public static void main(String[] args) {
+        DataSource dataSource = new DataSource();
+        if(!dataSource.open()) {
+            System.out.println("Can't open datasource");
+            return;
+        }
+
+        dataSource.close();
+    }
+}
---
+++ src/model/Song.java
@@ -0,0 +1,31 @@
+package model;
+
+public class Song {
+    private int id;
+    private int track;
+    private int albumId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getTrack() {
+        return track;
+    }
+
+    public void setTrack(int track) {
+        this.track = track;
+    }
+
+    public int getAlbumId() {
+        return albumId;
+    }
+
+    public void setAlbumId(int albumId) {
+        this.albumId = albumId;
+    }
+}
